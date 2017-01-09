684c266d817e6287a765a6e882df0bc1c52433d1
interstate80 <interstate80@yandex.ru>
2017-01-09 14:40:42 +0300

    1

---
+++ web/hello.py
@@ -0,0 +1,14 @@
+from cgi import parse_qs, escape
+
+def myapp(environ, start_response):
+    data = ''
+    p_data = parse_qs(environ['QUERY_STRING'])
+    b_data = p_data.get('b', [''])[0]
+    a_data = p_data.get('a', [])
+    for aa in a_data:
+        data = data + 'a=' + aa + '\n'
+    data = data + 'b=' + b_data + '\n'
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(data)))]
+    start_response(status, response_headers)
+    return [ data ]
