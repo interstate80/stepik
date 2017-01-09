d888c5444d84decae67ab1d4fd9fab22ae86f9e4
interstate80 <interstate80@yandex.ru>
2017-01-09 21:23:27 +0300

    1

--- web/hello.py
+++ web/hello.py
@@ -1,15 +1,14 @@
 #! /usr/bin/python3
 
-from cgi import parse_qs, escape
+# from cgi import parse_qs, escape
+from urllib.parse import parse_qs
 
 def myapp(environ, start_response):
     data = ''
     p_data = parse_qs(environ['QUERY_STRING'])
-    b_data = p_data.get('b', [''])[0]
-    a_data = p_data.get('a', [])
-    for aa in a_data:
-        data = data + 'a=' + aa + '\n'
-    data = data + 'b=' + b_data + '\n'
+    for i in p_data:
+        data += str(i) + "=" + str(p_data[i]) + "\n"
+    
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(data)))]
     start_response(status, response_headers)
