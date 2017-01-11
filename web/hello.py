8bf1d2489e578ac3072cf735572e7f84402a294b
interstate80 <interstate80@yandex.ru>
2017-01-11 20:01:46 +0300

    1

--- web/hello.py
+++ web/hello.py
@@ -1,13 +1,11 @@
 #! /usr/bin/python
 
-#from cgi import parse_qs
-import urlparse
-
-def myapp(env, start_response):
-    data = ''
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    p_data = env['QUERY_STRING'].split('&')
-
-    start_response(status, response_headers)
-    return ['%s' % k for k in p_data]
+def myapp(env, start_response):                                                                                      
+    data = ''                                                                                                        
+    status = '200 OK'                                                                                                
+    response_headers = [('Content-type', 'text/plain')]                                                              
+    p_data = env['QUERY_STRING']                                                                                     
+    for i in p_data.split('&'):                                                                                      
+        data += '%s\n' % i                                                                                           
+    start_response(status, response_headers)                                                                         
+    return [ data ]                                                                                                  
