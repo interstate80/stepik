6dd62e29804cb0a596d969e2aa9882e572248440
interstate80 <interstate80@yandex.ru>
2017-01-11 13:36:56 +0300

    1

--- web/hello.py
+++ web/hello.py
@@ -7,10 +7,7 @@ def myapp(env, start_response):
     data = ''
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
-    p_data = urlparse.parse_qs(env['QUERY_STRING'])
-    # for i, v in p_data.items():
-    #     v = str(v).replace('\']','')
-    #     v = v.replace('[\'','')
-    #     data += '%s=%s\n' % (i,v)
+    p_data = env['QUERY_STRING'].split('&')
+
     start_response(status, response_headers)
-    return ['%s=%s<br>' % (k, p_data[k][0]) for k in p_data]
+    return ['%s' % k for k in p_data]
